1f599e6e86229551d6cbb7da56b22fa0a092f3a4
Artem <carbon0093@gmail.com>
2021-08-09 14:46:26 +0300

    feature: подключил redux и react-redux, а так же dev tools

--- react-gb/src/App.js
+++ react-gb/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Router from './components/component/Router.js';
+import { store } from './store/index.js';
 
 function App() {
 
   return (
-    <Router />
+    <Provider store={store}>
+          <Router />
+    </Provider>
   );
 }
 
