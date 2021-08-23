3b8f3be2973145f00be77af350613432f6e39b81
Artem <carbon0093@gmail.com>
2021-08-23 14:20:09 +0300

    feature: добавил FB и сделал login

--- react-gb/src/index.js
+++ react-gb/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {initFB} from './services/index.js';
+
+initFB();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
